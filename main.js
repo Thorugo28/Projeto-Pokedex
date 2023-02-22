db2ca62284efd9d0653cab26e9a10f5f2834a763
Victor Hugo <victorhralmeida@gmail.com>
2023-02-22 13:57:58 -0300

    Versão 1.0 do Projeto

---
+++ main.js
@@ -0,0 +1,97 @@
+const pokemonName = document.querySelector(".pokemon__name")
+const pokemonNumber = document.querySelector(".pokemon__number")
+const pokemonImage = document.querySelector(".pokemon__imagem")
+const types = document.querySelector("#types")
+
+const form = document.querySelector(".form")
+const input = document.querySelector(".input__search")
+const buttonPrev = document.querySelector('.btn__prev');
+const buttonNext = document.querySelector('.btn__nxt');
+
+const typeColors = {
+    "rock":     [182, 158,  49],
+    "ghost":    [112,  85, 155],
+    "steel":    [183, 185, 208],
+    "water":    [100, 147, 235],
+    "grass":    [116, 203,  72],
+    "psychic":  [251,  85, 132],
+    "ice":      [154, 214, 223],
+    "dark":     [117,  87,  76],
+    "fairy":    [230, 158, 172],
+    "normal":   [170, 166, 127],
+    "fighting": [193,  34,  57],
+    "flying":   [168, 145, 236],
+    "poison":   [164,  62, 158],
+    "ground":   [222, 193, 107],
+    "bug":      [167, 183,  35],
+    "fire":     [245, 125,  49],
+    "electric": [249, 207,  48],
+    "dragon":   [112,  55, 255]
+}
+
+
+let searchPokemon = 1;
+
+const fetchPokemon = async (pokemon) => {
+  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+  if (APIResponse.status === 200) {
+    const data = await APIResponse.json();
+    return data;
+  }
+}
+
+const renderPokemon = async (pokemon) => {
+
+  pokemonName.innerHTML = 'Loading...';
+  pokemonNumber.innerHTML = '';
+
+  const data = await fetchPokemon(pokemon);
+
+  if (data) {
+    pokemonImage.style.display = 'block';
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    input.value = '';
+    searchPokemon = data.id;
+  } else {
+    pokemonImage.style.display = 'none';
+    pokemonName.innerHTML = 'Not found :c';
+    pokemonNumber.innerHTML = '';
+  }
+
+  types.innerHTML = '';
+
+  data.types.forEach((t) => {
+    let newType = document.createElement('span');
+    let color   = typeColors[t.type.name];
+
+    newType.innerHTML = t.type.name;
+    newType.classList.add('type');
+    newType.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`; 
+
+    types.appendChild(newType);
+});
+}
+
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  renderPokemon(input.value.toLowerCase());
+});
+
+buttonPrev.addEventListener('click', () => {
+  if (searchPokemon > 1) {
+    searchPokemon -= 1;
+    renderPokemon(searchPokemon);
+  }
+});
+
+buttonNext.addEventListener('click', () => {
+  searchPokemon += 1;
+  renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
+
